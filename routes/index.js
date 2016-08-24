522db412acf6664040b7b366686dba6da996a570
Li Zhenyu <icytornado@163.com>
2016-08-24 18:02:58 +0800

    Add post a blog

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,21 @@ var express = require('express');
 var router = express.Router();
 var crypto = require('crypto');
 var User = require('../models/User');
+var Post = require('../models/Post');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: '首页' });
+    Post.get(null, function(err, posts) {
+        if (err) {
+            posts = [];
+        }
+
+        console.log("WTF"+posts.length);
+        res.render('index', {
+            title: '首页',
+            posts: posts,
+        });
+    });
 });
 // GET register page
 router.get('/reg',checkNotLogin);
@@ -78,6 +89,41 @@ router.get('/logout', function(req, res) {
     req.flash('success', '登出成功');
     res.redirect('/');
 });
+//POST post page
+router.post('/post', checkLogin);
+router.post('/post', function(req, res) {
+    var currentUser = req.session.user;
+    var post = new Post(currentUser.name, req.body.post);
+    post.save(function(err) {
+        if (err) {
+            req.flash('error', err);
+            return res.redirect('/');
+        }
+        req.flash('success', '发表成功');
+        res.redirect('/u/' + currentUser.name);
+    });
+});
+//GET user page
+router.get('/u/:user', function(req, res) {
+    User.get(req.params.user, function(err, user) {
+        if (!user) {
+            req.flash('error', '用户不存在');
+            return res.redirect('/');
+        }
+        Post.get(user.name, function(err, posts) {
+            if (err) {
+                req.flash('error', err);
+                return res.redirect('/');
+            }
+            res.render('user', {
+                title: user.name,
+                posts: posts
+            });
+        });
+    });
+});
+
+
 
 function checkLogin(req, res, next) {
     if (!req.session.user) {
